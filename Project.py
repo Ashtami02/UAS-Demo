e33557f41ce899c89fc9b31d42e8df6ff671ff69
Ashtami <ashtami>
2025-08-30 20:40:56 +0530

    update

---
+++ Project.py
@@ -0,0 +1,20 @@
+#IMPORTING THE REQUIRED LIBRARIES
+
+
+import numpy as np
+import cv2
+import sys
+import matplotlib
+
+
+#WRITING THE CODE FOR LAND AND OCEAN SEGREGATION
+
+l=["1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png"]
+for i in l:
+    img= cv2.imread(i)
+    cv2.imshow("image",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    print(img.shape)
+    cv2.line(img, (100, 100), (400, 400), (0, 0, 255), 5)
+
